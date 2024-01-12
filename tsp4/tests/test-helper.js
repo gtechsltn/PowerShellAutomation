03abd505356587ef52e8052324c3f02f1b100bf7
Nguyen Viet Manh <manh.nguyen@thirdsight.net>
2024-01-12 17:31:41 +0800

    tsp3, tsp4, tsp5

---
+++ tsp4/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'tsp4/app';
+import config from 'tsp4/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
